7321539a3de8f727fd4cc6868fe42c84781fb1be
Fabio Mendes <fabiojmendes@gmail.com>
2024-08-19 01:03:35 -0400

    Adding build information at runtime

--- build.rs
+++ build.rs
@@ -2,4 +2,5 @@
 fn main() {
     // trigger recompilation when a new migration is added
     println!("cargo:rerun-if-changed=migrations");
+    built::write_built_file().expect("Failed to acquire build-time information");
 }
--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,22 @@ use std::env;
 async fn main() -> anyhow::Result<()> {
     logging::init();
     tracing::info!("Starting Server");
+    tracing::info!(
+        "Doorsys api version {}, built for {} by {}.",
+        built_info::PKG_VERSION,
+        built_info::TARGET,
+        built_info::RUSTC_VERSION
+    );
+    if let (Some(version), Some(hash), Some(dirty)) = (
+        built_info::GIT_VERSION,
+        built_info::GIT_COMMIT_HASH_SHORT,
+        built_info::GIT_DIRTY,
+    ) {
+        tracing::info!("Git version: {version} ({hash})");
+        if dirty {
+            tracing::warn!("Repo was dirty");
+        }
+    }
 
     let pool = PgPoolOptions::new()
         .min_connections(5)
@@ -23,4 +39,8 @@ async fn main() -> anyhow::Result<()> {
     let mqtt_client = mqtt::start(pool.clone(), &mqtt_url).await?;
 
     http::serve(pool, mqtt_client).await
+}
+
+pub mod built_info {
+    include!(concat!(env!("OUT_DIR"), "/built.rs"));
 }
